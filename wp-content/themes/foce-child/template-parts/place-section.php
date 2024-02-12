b2638a5a442770b9a14c339d4f5eeffaabd1f431
Louise Chartier <louise-chartier@outlook.fr>
2024-02-12 18:02:09 +0100

    change clouds direction

--- wp-content/themes/foce-child/template-parts/place-section.php
+++ wp-content/themes/foce-child/template-parts/place-section.php
@@ -1,5 +1,5 @@
 <article id="place" class="animation_container">
-    <div id="cloud_animation"  data-1300="left:0%;"  data-2190="left:30%";>
+    <div id="cloud_animation"  data-1300="right:0px;"  data-2190="right:300px";>
         <img class="clouds" id="little_cloud" src="wp-content\themes\foce-child\assets\images\little_cloud.png" alt="little cloud">
         <img class="clouds"  id="big_cloud" src="wp-content\themes\foce-child\assets\images\big_cloud.png" alt="big cloud"></div>
                 <div >
